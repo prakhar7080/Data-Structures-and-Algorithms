590bd51fe8bc011b591a0f3e784e1fb0a3ba7995
Prakhar Gupta <prakhargupta3510@gmail.com>
2025-03-17 22:30:46 +0530

    Update largest_element_in_array.cpp

--- Array/largest_element_in_array.cpp
+++ Array/largest_element_in_array.cpp
@@ -1,1 +1,21 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter size of array : ";
+    cin>>n;
+    int* arr = new int[n];
+    cout<<"Enter array elements : ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    int max = arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    cout<<"Maximum element : "<<max;
+    delete[] arr;
+}
